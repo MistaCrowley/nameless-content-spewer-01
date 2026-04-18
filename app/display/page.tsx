f7a398edb570a80c7f96a29abeea75299d15d33f
MistaCrowley <g14n7.5qu1d@gmail.com>
2026-04-18 00:27:55 -0400

    locals 10 yeye

--- app/display/page.tsx
+++ app/display/page.tsx
@@ -84,7 +84,7 @@ key="my-animated-div" // Important: Direct children of AnimatePresence must have
       ">
       <Suspense>
         <ContentScroller 
-          image={"oscillate3"}
+          image={"localsonly9"}
           tellParentHideSideLoader={hideSideLoader}
           routeRules={routeRules}
           />
